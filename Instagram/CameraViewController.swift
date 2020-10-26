7e7d220d2a103add214c2a2835a69d864a8a7e39
aquaomarine <rajeneharris@hotmail.com>
2020-10-26 01:33:58 -0400

    first save

--- Instagram/CameraViewController.swift
+++ Instagram/CameraViewController.swift
@@ -6,16 +6,44 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var imageView: UIImageView!
     
-
+    @IBAction func commentView(_ sender: Any) {
+    }
+    @IBAction func onSubmitButton(_ sender: Any) {
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
--- Podfile
+++ Podfile
@@ -1,18 +1,23 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '9.0'
 target 'Instagram' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
-
+  
   # Pods for Instagram
   pod 'Parse'
+  pod 'Alamofire'
+
+  # Pods for testing
   target 'InstagramTests' do
     inherit! :search_paths
-    # Pods for testing
+    pod 'Alamofire'
   end
 
   target 'InstagramUITests' do
     # Pods for testing
+    inherit! :search_paths
+    pod 'Alamofire'
   end
 
 end
